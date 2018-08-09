ebeed0a4eaaa93187182c2559e9391cd928056f6
Sumit Lakra <sumitlakradev@gmail.com>
2018-08-09 17:39:26 +0530

    Implementation of mailboxes based on IFFQ (Improved FastForward Queues)

---
+++ sys/net/pspat/mailbox.c
@@ -0,0 +1,84 @@
+#include "mailbox.h"
+
+#define is_power_of_2(x)	((x) != 0 && (((x) & ((x) - 1)) == 0))
+
+MALLOC_DEFINE(M_MB, "mailbox", "IFFQ Mailbox Implementation");
+
+int
+pspat_mb_new(const char *name, unsigned long entries, unsigned long line_size,
+		struct pspat_mailbox **m)
+{
+	int err;
+
+	*m = malloc(pspat_mb_size(entries), M_MB, M_WAITOK | M_ZERO);
+	if (*m == NULL)
+		return -ENOMEM;
+
+	err = pspat_mb_init(*m, name, entries, line_size);
+	if (err) {
+		free(*m, M_MB);
+		return err;
+	}
+
+	return 0;
+}
+
+int
+pspat_mb_init(struct pspat_mailbox *m, const char *name,
+		unsigned long entries, unsigned long line_size)
+{
+	unsigned long entries_per_line;
+
+	if (!is_power_of_2(entries) || !is_power_of_2(line_size) ||
+			entries * sizeof(void *) <= 2 * line_size || line_size < sizeof(void *))
+		return -EINVAL;
+
+	snprintf(m->name, PSPAT_MB_NAMSZ, "%s", name);
+	m->name[PSPAT_MB_NAMSZ - 1 ] = '\0';
+
+	entries_per_line = line_size / sizeof(void *);
+
+	m->line_entries = entries_per_line;
+	m->line_mask = ~(entries_per_line - 1);
+	m->entry_mask = entries - 1;
+
+#ifdef PSPAT_MB_DEBUG
+	printf("PSPAT: mb %p %s: line_entries %lu line_mask %lx entry_mask %lx\n",
+		m, m->name, m->line_entries, m->line_mask, m->entry_mask);
+#endif
+
+	m->cons_clear = 0;
+	m->cons_read = m->line_entries;
+	m->prod_write = m->line_entries;
+	m->prod_check = m->line_entries;
+
+	unsigned initial_clear = m->cons_clear;
+	/* Fill the first cacheline with a garbage value */
+	while(initial_clear != m->cons_read) {
+		m->q[initial_clear & m->entry_mask] = (void *) 1;
+		initial_clear++;
+	}
+
+	/* Initialize the TAILQ list entry */
+	ENTRY_INIT(&m->entry);
+	m->entry.mb = m;
+
+	return 0;
+}
+
+void
+pspat_mb_delete(struct pspat_mailbox *m)
+{
+#ifdef PSPAT_MB_DEBUG
+	printf("PSPAT: deleting mb %s\n", m->name);
+#endif
+	free(m, M_MB);
+}
+
+
+void
+pspat_mb_dump_state(struct pspat_mailbox *m)
+{
+	printf("%s: cc %lu, cr %lu, pw %lu, pc %lu\n", m->name,
+		m->cons_clear, m->cons_read, m->prod_write, m->prod_check);
+}
---
+++ sys/net/pspat/mailbox.h
@@ -0,0 +1,166 @@
+#ifndef __PSPAT_MAILBOX_H
+#define __PSPAT_MAILBOX_H
+
+#include <sys/param.h>
+#include <sys/kernel.h>
+#include <sys/systm.h>
+#include <sys/malloc.h>
+#include <sys/queue.h>
+
+#define PSPAT_MB_NAMSZ	32
+#define PSPAT_MB_DEBUG 1
+
+#define ENTRY_EMPTY(entry) ((entry.entries.tqe_next == NULL) && ((entry.entries.tqe_prev) == NULL))
+
+struct list {
+	TAILQ_ENTRY(list)	entries;
+	void *mb; /* Pointer to store address of the containing mailbox */
+};
+
+static inline void ENTRY_INIT (struct list *entry) {
+	entry->entries.tqe_prev = NULL;
+	entry->entries.tqe_next = NULL;
+}
+
+TAILQ_HEAD(entry_list, list);
+
+MALLOC_DECLARE(M_MB);
+
+struct pspat_mailbox {
+	/* shared (constant) fields */
+	char			name[PSPAT_MB_NAMSZ];
+	unsigned long		entry_mask;
+	unsigned long		line_entries;
+	unsigned long		line_mask;
+
+	/* shared field, written by both */
+	unsigned long		backpressure;
+	int			dead; /* written by producer */
+	unsigned int		identifier;
+
+	/* producer fields */
+	unsigned long		prod_write __aligned(CACHE_LINE_SIZE);
+	unsigned long		prod_check;
+
+	/* consumer fields */
+	unsigned long		cons_clear __aligned(CACHE_LINE_SIZE);
+	unsigned long		cons_read;
+
+	/* TAILQ of mailboxes*/
+	struct list entry;
+
+	/* the queue */
+	void *		q[0] __aligned(CACHE_LINE_SIZE);
+};
+
+static inline size_t pspat_mb_size(unsigned long entries)
+{
+	return roundup(sizeof(struct pspat_mailbox) + entries * sizeof(void *),
+			CACHE_LINE_SIZE);
+}
+
+/**
+ * pspat_mb_new - create a new mailbox
+ * @name: an arbitrary name for the mailbox (debug)
+ * @entries: the number of entries
+ * @line_size: the line size in bytes
+ *
+ * Both entries and line_size must be a power of 2.
+ * Returned pointer must be checked with IS_ERR().
+ */
+int pspat_mb_new(const char *name, unsigned long entries,
+		unsigned long line_size, struct pspat_mailbox **m);
+
+/**
+ * pspat_mb_init - initialize a pre-allocated mailbox
+ * @m: the mailbox to be initialized
+ * @name: an arbitrary name for the mailbox (debug)
+ * @entries: the number of entries
+ * @line_size: the line size in bytes
+ *
+ * Both entries and line_size must be a power of 2.
+ * Returns 0 on success, -errno on failure.
+ */
+int pspat_mb_init(struct pspat_mailbox *m, const char *name, unsigned long entries,
+		unsigned long line_size);
+
+/**
+ * pspat_mb_delete - delete a mailbox
+ * @m: the mailbox to be deleted
+ */
+void pspat_mb_delete(struct pspat_mailbox *m);
+
+void pspat_mb_dump_state(struct pspat_mailbox *m);
+
+/**
+ * pspat_mb_insert - enqueue a new value
+ * @m: the mailbox where to enqueue
+ * @v: the value to be enqueued
+ *
+ * Returns 0 on success, -ENOBUFS on failure.
+ */
+static inline int pspat_mb_insert(struct pspat_mailbox *m, void *v)
+{
+	void *h = &m->q[m->prod_write & m->entry_mask];
+	if (m->prod_write == m->prod_check) {
+		/* Leave a cache line empty. */
+		if (m->q[(m->prod_check + m->line_entries) & m->entry_mask])
+			return -ENOBUFS;
+		m->prod_check += m->line_entries;
+		__builtin_prefetch((char *)h + m->line_entries);
+	}
+	m->q[m->prod_write & m->entry_mask] = v;
+	m->prod_write++;
+	return 0;
+}
+
+/**
+ * pspat_mb_empty - test for an empty mailbox
+ * @m: the mailbox to test
+ *
+ * Returns non-zero if the mailbox is empty
+ */
+static inline int pspat_mb_empty(struct pspat_mailbox *m)
+{
+	void *v = m->q[m->cons_read & m->entry_mask];
+	return (!v);
+}
+
+/**
+ * pspat_mb_extract - extract a value
+ * @m: the mailbox where to extract from
+ * 
+ * Returns the extracted value, NULL if the mailbox
+ * is empty. It does not free up any entry, use
+ * pspat_mb_clear for that
+ */
+static inline void *pspat_mb_extract(struct pspat_mailbox *m)
+{
+	void *v = m->q[m->cons_read & m->entry_mask];
+	if (!v)
+		return NULL;
+	m->cons_read++;
+	return v;
+}
+
+/**
+ * pspat_mb_clear - clear the previously extracted entries
+ * @m: the mailbox to be cleared
+ *
+ */
+static inline void pspat_mb_clear(struct pspat_mailbox *m)
+{
+	unsigned next_clear = (m->cons_read & m->line_mask) - m->line_entries;
+
+	while(m->cons_clear != next_clear) {
+		m->q[m->cons_clear & m->entry_mask] = 0;
+		m->cons_clear++;
+	}
+}
+
+static inline void pspat_mb_prefetch(struct pspat_mailbox *m)
+{
+	__builtin_prefetch((void *)m->q[m->cons_read & m->entry_mask]);
+}
+
+#endif /* __PSPAT_MAILBOX_H */
